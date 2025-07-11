eadb65af31776db2f498340c410b47287e80f3cb
mvacjar <mvacjar@gmail.com>
2025-07-11 19:04:36 +0200

    Images added in assets folder and a index.js for a better access to the images when I import them

---
+++ client/app/assets/index.js
@@ -0,0 +1,12 @@
+import LogoLight from './images/LogoLight.svg?react';
+import amazon from './images/amazon_snake.png';
+
+const Icon = {
+  LogoLight,
+};
+
+const image = {
+  amazon,
+};
+
+export { Icon, image };
